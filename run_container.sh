84f9393bae1f0240eab15d42abb7911bdb05a993
Alex Gonzalez <alexg@balena.io>
2025-02-27 11:21:15 +0100

    run_container: use prebuilt images
    Change-type: patch
    Signed-off-by: Alex Gonzalez <alexg@balena.io>

--- run_container.sh
+++ run_container.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
-
 set -e
 
 . ./container/helpers
 
-# Standard container, if dram and image are not passed directly to this script
-cmd="docker container run --rm -it --privileged -v /dev/:/dev/ -v ~/images:/data/images uuu-image /bin/bash"
 
 if ! command -v docker > /dev/null
 then
 	log ERROR "Docker command not found. At least Docker v20 needs to be installed on your Host....."
 fi
-
 
 # Parse arguments
 while [[ $# -gt 0 ]]; do
@@ -44,24 +40,29 @@ while [[ $# -gt 0 ]]; do
 	shift
 done
 
-# if -i arg is passed, run the flasher script in container directly
-if [ ! -z ${balena_image+x} ]; then
-	imageName=`basename ${balena_image}`
-	cmd="docker container run --rm -it --privileged -v /dev/:/dev/ -v ${balena_image}:/usr/src/app/${imageName} uuu-image /bin/bash ./flash_iot.sh -i /usr/src/app/${imageName}"
-	log "Provisioning process will start now."
-fi
-
-
+platform="linux/amd64"
 if [[ ${arch} = "armv7" ]]; then
 	log "Will build flash container for armv7..."
 	imageTag="--build-arg RT=armv7hf-ubuntu:focal-run-20221215"
+	platform="linux/arm/v7"
 elif [[ ${arch} = "aarch64" ]]; then
 	log "Will build flash container for aarch64..."
 	imageTag="--build-arg RT=aarch64-ubuntu:focal-run-20221215"
+	platform="linux/arm64/v8"
+fi
+
+containerImage="uuu-image-${arch}"
+# Standard container, if dram and image are not passed directly to this script
+cmd="docker container run --platform ${platform} --rm -it --privileged -v /dev/:/dev/ -v ~/images:/data/images ${containerImage} /bin/bash"
+# if -i arg is passed, run the flasher script in container directly
+if [ ! -z ${balena_image+x} ]; then
+	imageName=`basename ${balena_image}`
+	cmd="docker container run --platform ${platform} --rm -it --privileged -v /dev/:/dev/ -v ${balena_image}:/usr/src/app/${imageName} ${containerImage} /bin/bash ./flash_iot.sh -i /usr/src/app/${imageName}"
+	log "Provisioning process will start now."
 fi
 
 # Build Dockerfile, if image does not exist already
-docker build -t uuu-image . ${imageTag}
+docker pull --platform "${platform}" "${containerImage}" || docker build -t ${containerImage} . ${imageTag}
 
 eval $cmd
 
